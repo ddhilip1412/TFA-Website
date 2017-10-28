a83c73cfebc235026ea1f289615b494f9a66b8f0
ddhilip1412 <ddhilip1412@gmail.com>
2017-10-28 10:32:40 +0530

    Refactored code
    Added scrolloverlay for top and bottom of the dropdown.

--- js/tfa-website.js
+++ js/tfa-website.js
@@ -1,16 +1,19 @@
-$(document).ready(function()
-{
-   $('#listcourses').scroll(function()
-   {
-      var dropdownlist = $(this);
-      var dropdownoverlay = dropdownlist.find($('.dropdownoverlay'))
-      if (dropdownlist.height() <  (dropdownlist.scrollTop()-142)) //142 is based on the height of the dropdown list in small screens. It is set to 250px in the css class "navbardropdown" inside customstyle.css. Should be fixed since this is a hack.
-      {
-      	dropdownoverlay.css("display","none");
-      } else
-      {
-      	  dropdownoverlay.css("display","block");
-      }
+$(document).ready(function() {
+    $('#listcourses').scroll(function() {
+        var dropdownlist = $(this);
+        var scrolloverlaytop = dropdownlist.find($('.scrolloverlaytop'));
+        var scrolloverlaybottom = dropdownlist.find($('.scrolloverlaybottom'));
+        if (dropdownlist.height() < (dropdownlist.scrollTop() - 142)) //142 is based on the height of the dropdown list in small screens. It is set to 250px in the css class "navbardropdown" inside customstyle.css. Should be fixed since this is a hack.
+        {
+            scrolloverlaybottom.css("display", "none");
+        } else {
+            scrolloverlaybottom.css("display", "block");
+        }
+        if (dropdownlist.scrollTop() > 0) {
+            scrolloverlaytop.css("display", "block");
+        } else {
+            scrolloverlaytop.css("display", "none");
+        }
 
-   });
+    });
 });
